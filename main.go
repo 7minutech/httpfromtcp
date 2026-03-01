2ebf4effc045f7d7418b96bd2bff53b392571326
7minutech <elijohnson1107@gmail.com>
2026-02-28 22:20:52 -0600

    feat: read from file in 8 bytes at a time

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
+const READ_SIZE = 8
+
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatalf("failed to open message file: %v", err)
+	}
+	defer file.Close()
+
+	var buffer []byte = make([]byte, READ_SIZE)
+	for {
+		n, err := file.Read(buffer)
+		if errors.Is(err, io.EOF) {
+			os.Exit(0)
+		}
+		if err != nil {
+			log.Fatalf("failed to read from message file: %v", err)
+		}
+		fmt.Printf("read: %s\n", string(buffer[:n]))
+	}
 }
